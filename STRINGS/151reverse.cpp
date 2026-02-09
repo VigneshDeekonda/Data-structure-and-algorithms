fd9b51c1caba61d83223d8f24ed0f6a593529f3a
Vignesh Deekonda <vignesh.deekonda5002@gmail.com>
2026-02-10 00:56:42 +0530

    Added single number solution and updated reverse string

--- STRINGS/151reverse.cpp
+++ STRINGS/151reverse.cpp
@@ -1,41 +1,41 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Solution
-// {
-// public:
-//     string stringreversal(string s)
-//     {
-//         int n = s.size();
-//         string ans = "";
+class Solution
+{
+public:
+    string stringreversal(string s)
+    {
+        int n = s.size();
+        string ans = "";
 
-//         reverse(s.begin(), s.end());
+        reverse(s.begin(), s.end());
 
-//         for (int i = 0; i < n; i++)
-//         {
-//             string word = "";
+        for (int i = 0; i < n; i++)
+        {
+            string word = "";
 
-//             while (i < n && s[i] != ' ')
-//             {
-//                 word += s[i];
-//                 i++;
-//             }
-//             reverse(word.begin(), word.end());
-//             if (word.length() > 0)
-//             {
-//                 ans += word + " ";
-//             }
-//         }
-//         ans.pop_back(); // remove last space
-//         return ans;
-//     }
-// };
+            while (i < n && s[i] != ' ')
+            {
+                word += s[i];
+                i++;
+            }
+            reverse(word.begin(), word.end());
+            if (word.length() > 0)
+            {
+                ans += word + " ";
+            }
+        }
+        ans.pop_back(); // remove last space
+        return ans;
+    }
+};
 
-// int main()
-// {
-//     string s = " vignesh deekonda";
-//     Solution ob;
-//    cout << ob.stringreversal(s);
-//    return 0;
-// }
+int main()
+{
+    string s = " vignesh deekonda";
+    Solution ob;
+   cout << ob.stringreversal(s);
+   return 0;
+}
 
